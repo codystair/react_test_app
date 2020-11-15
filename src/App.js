5c90c68fafc33add67d5907f718656cef36703ff
Cody Stair <ccstair27@gmail.com>
2020-11-14 19:30:25 -0500

    first change

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Changes!
         </p>
+        <h2>I love my mom!</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
